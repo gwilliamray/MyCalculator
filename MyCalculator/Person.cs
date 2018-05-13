072d8e4d0914b37a26202af68afd14e8f5452c24
Glenn W. Ray <glennr@radiks.net>
2018-05-13 13:39:54 -0500

    Add the phone field

--- MyCalculator/Person.cs
+++ MyCalculator/Person.cs
@@ -11,6 +11,7 @@ namespace MyCalculator
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
+        public string Phone { get; set; }
 
 
     }
