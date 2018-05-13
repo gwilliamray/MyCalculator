be1c3bb7f442fa9f7d6708a68ef78dff6ce37204
Glenn W. Ray <glennr@radiks.net>
2018-05-13 13:30:03 -0500

    Added the address field to the Person

--- MyCalculator/Person.cs
+++ MyCalculator/Person.cs
@@ -10,6 +10,8 @@ namespace MyCalculator
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Address { get; set; }
+
 
     }
 }
